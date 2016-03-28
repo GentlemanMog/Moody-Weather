10e412902d096409a520cc13c1b6b9b9e6723e80
GentlemanMog <GentlemanMog@gmail.com>
2016-03-29 00:20:28 +1300

    LightSensor Test
    Using Light Sensors

--- data/js/weatherApp.js
+++ data/js/weatherApp.js
@@ -6,13 +6,14 @@ else{
 }
 
 $(".getLocation").on('click', function(){
-navigator.geolocation.getCurrentPosition(function(position){
+	navigator.geolocation.getCurrentPosition(function(position){
 	loadWeather(position.coords.latitude + ',' + position.coords.longitude);
 	});
 });
 
 $(document).ready(function(){
-	loadWeather('Auckland', '');
+	loadWeather('London', '');
+	setInterval(loadWeather, 300000);
 });
 
 function loadWeather(location, woeid){
@@ -21,7 +22,39 @@ function loadWeather(location, woeid){
 		woeid: woeid,
 		unit: 'f',
 		success: function(weather){
-			html = '<h2><i class="icon-' + weather.code + '"></i>' + weather.temp + '&deg;' + weather.units.temp + '</h2>';
+			if(weather.alt.temp > 10){
+				$('.MoodRing').animate({
+					backgroundColor: 'blue'
+				}, 1500);
+			}else if (weather.alt.temp > 20) {
+				$('.MoodRing').animate({
+					backgroundColor: 'yellow'
+				}, 1500);
+			}else{
+				$('.MoodRing').animate({
+					backgroundColor: 'crimson'
+				}, 1500);
+			}
+			if(weather.humidity > 40){
+				$('.humidMood').animate({
+					backgroundColor: 'blue'
+				}, 1500);
+			}else if (weather.humidity > 60) {
+				$('.humidMood').animate({
+					backgroundColor: 'yellow'
+				}, 1500);
+			}else{
+				$('.humidMood').animate({
+					backgroundColor: 'crimson'
+				}, 1500);
+			}
+
+			html = '<h2><i class="icon-' + weather.code + '"></i>' + weather.alt.temp + '&deg;C' + /*weather.units.temp + */'</h2>';
+			
+
+			// var timestamp = moment(weather.updated);
+			// html += '<p class="updated"> Updated' + moment(timestamp).fromNow() + '<p>';
+
 			$("#weatherTime").html(html);
 		},
 		error: function(error){
